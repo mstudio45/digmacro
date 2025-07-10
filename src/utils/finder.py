3e69fd8ea5b5b6dcc686fcd1d11c9cb9411e92f9
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2025-07-10 15:42:36 +0200

    revert

--- src/utils/finder.py
+++ src/utils/finder.py
@@ -126,36 +126,36 @@ class PlayerBar:
         self.mask = mask
 
         # find player bar #
-        #if detection == "Sobel":
-        candidate_bars = []
+        if detection == "Sobel":
+            candidate_bars = []
 
-        for cnt in contours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            if w >= 1 and h > 15 and h / w > 5:
-                candidate_bars.append((x, y, w, h))
+            for cnt in contours:
+                x, y, w, h = cv2.boundingRect(cnt)
+                if w >= 1 and h > 15 and h / w > 5:
+                    candidate_bars.append((x, y, w, h))
 
-        candidate_bars.sort(key=lambda b: b[0])
+            candidate_bars.sort(key=lambda b: b[0])
 
-        for i in range(len(candidate_bars) - 1):
-            x1, y1, w1, h1 = candidate_bars[i]
-            x2, y2, w2, h2 = candidate_bars[i + 1]
+            for i in range(len(candidate_bars) - 1):
+                x1, y1, w1, h1 = candidate_bars[i]
+                x2, y2, w2, h2 = candidate_bars[i + 1]
 
-            if w1 == w2 and h1 == h2:
-                if 0 < x2 - (x1 + w1) <= self.distance_threshold:
-                    fixed_x = region_left + (x1 + w1 // 2) + 5
+                if w1 == w2 and h1 == h2:
+                    if 0 < x2 - (x1 + w1) <= self.distance_threshold:
+                        fixed_x = region_left + (x1 + w1 // 2) + 5
+
+                        player_bar_bbox = (fixed_x, y, Config.PLAYER_BAR_WIDTH, region_height)
+                        player_bar_center = fixed_x
+                        break
+        else:
+            for cnt in contours:
+                x, y, w, h = cv2.boundingRect(cnt)
+                if w >= 1 and h > 15 and h / w > 5:
+                    fixed_x = region_left + (x + w // 2) - 5
 
                     player_bar_bbox = (fixed_x, y, Config.PLAYER_BAR_WIDTH, region_height)
                     player_bar_center = fixed_x
                     break
-    #else:
-        #    for cnt in contours:
-        #        x, y, w, h = cv2.boundingRect(cnt)
-        #        if w >= 1 and h > 15 and h / w > 5:
-        #            fixed_x = region_left + (x + w // 2) - 5
-#
-        #            player_bar_bbox = (fixed_x, y, Config.PLAYER_BAR_WIDTH, region_height)
-        #            player_bar_center = fixed_x
-        #            break
                     
         if not player_bar_center:
             self.position = None
