973f74d18602896e7901173d347c56061eb421fe
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2025-07-18 16:07:22 +0200

    crlf :sob:

--- scripts/remove_pycache.sh
+++ scripts/remove_pycache.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Deleting all __pycache__ folders recursively from $(pwd) ..."
-find . -type d -name "__pycache__" -print -exec rm -rf {} +
+find . -type d | grep '__pycache__$' | xargs rm -rf
 echo "Done!"
